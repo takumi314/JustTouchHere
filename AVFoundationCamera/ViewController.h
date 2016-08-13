9dc32e45c875e59d6565b86b29e8d977b1f6ef98
takumi314 <isospin_symmetry@me.com>
2016-08-14 02:49:47 +0900

    Add two files

---
+++ AVFoundationCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundationCamera
+//
+//  Created by NishiokaKohei on 2016/08/14.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
